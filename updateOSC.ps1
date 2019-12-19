31aeee1283863b50db5971acc8212670504009e1
Oscar Manglaras <cogent.redtester@outlook.com>
2019-12-19 12:02:48 +1030

    added script to download a new version of the osc, and add lines to update layout

---
+++ updateOSC.ps1
@@ -0,0 +1,17 @@
+$download_file = (Get-Location).Path + "\scripts\oscAuto.lua"
+
+Write-Host "Downloading osc.lua from https://raw.githubusercontent.com/mpv-player/mpv/master/player/lua/osc.lua" -ForegroundColor Green
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mpv-player/mpv/master/player/lua/osc.lua" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile $download_file
+
+$new_text = "
+
+--automatically generated function to update options
+function update_opts()
+    opt.read_options(user_opts, 'osc')
+    visibility_mode(user_opts.visibility, true)
+    request_init()
+end
+
+mp.register_script_message('update-osc-options', update_opts)"
+
+$new_text | Add-Content $download_file
